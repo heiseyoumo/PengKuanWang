a74010675ad662280ae04a0cf540d3c44d8d126a
fancy <274737258@qq.com>
2019-07-16 18:18:40 +0800

    优化代码

--- app/src/main/java/com/fancy/myapplication/ui/Demo22Activity.java
+++ app/src/main/java/com/fancy/myapplication/ui/Demo22Activity.java
@@ -3,6 +3,7 @@ package com.fancy.myapplication.ui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.fancy.myapplication.R;
 
@@ -24,16 +25,18 @@ public class Demo22Activity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demo22);
         // RxJava的链式操作
-        Observable.create(new ObservableOnSubscribe<Integer>() {
+        final Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
             // 1. 创建被观察者(Observable) & 定义需发送的事件
             @Override
             public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                //emitter.setDisposable(DisposableHelper.DISPOSED);
                 emitter.onNext(1);
                 emitter.onNext(2);
                 emitter.onNext(3);
                 emitter.onComplete();
             }
-        }).subscribe(new Observer<Integer>() {
+        });
+        final Observer<Integer> observer = new Observer<Integer>() {
             // 2. 创建观察者(Observer) & 定义响应事件的行为
             // 3. 通过订阅（subscribe）连接观察者和被观察者
             @Override
@@ -55,6 +58,12 @@ public class Demo22Activity extends Activity {
             @Override
             public void onComplete() {
                 Log.d(TAG, "对Complete事件作出响应");
+            }
+        };
+        findViewById(R.id.textView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                observable.subscribe(observer);
             }
         });
     }
